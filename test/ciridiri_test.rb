3d2c21815dad7653d3e9cd13363c5fab4e157790
Vasily Polovnyov <vast@whiteants.net>
2010-05-13 17:22:48 +0400

    reformatted and refactored tests

--- test/ciridiri_test.rb
+++ test/ciridiri_test.rb
@@ -8,77 +8,79 @@ set :root, File.expand_path("../", File.dirname(__FILE__))
 
 class CiridiriTest < Test::Unit::TestCase
   include Rack::Test::Methods
-  def app
-    Sinatra::Application
-  end
+  describe "Ciridiri web-app" do
+    def app
+      Sinatra::Application
+    end
 
-  it "should redirect from root to index" do
-    get '/'
-    assert_redirect('/index.html')
-  end
+    should "redirect from root to index" do
+      get '/'
+      assert_redirect('/index.html')
+    end
 
-  it "should get existent page" do
-    page = page_stub
-    page.save
+    should "get existent page" do
+      page = page_stub
+      page.save
 
-    get "#{page.uri}.html"
-    assert last_response.ok?
-    assert last_response.body.include?(page.title)
-  end
+      get "#{page.uri}.html"
+      assert last_response.ok?
+      assert last_response.body.include?(page.title)
+    end
 
-  it "should redirect to edit form if page not found" do
-    get "/nonexistent.html"
-    assert_redirect("/nonexistent.html.e")
-  end
+    should "redirect to edit form if page not found" do
+      get "/nonexistent.html"
+      assert_redirect("/nonexistent.html.e")
+    end
 
-  it "should show an empty edit form" do
-    get "/nonexistent.html.e"
-    assert last_response.ok?
-    assert last_response.body.include?("form")
-  end
+    should "show an empty edit form" do
+      get "/nonexistent.html.e"
+      assert last_response.ok?
+      assert last_response.body.include?("form")
+    end
 
-  it "should create a new page" do
-    post "/foo.html", :contents => 'fut-fut-fut, freeeeestylo'
-    assert_redirect("/foo.html")
-    follow_redirect!
-    assert last_response.ok?
-    assert last_response.body.include?('freeeeestylo')
-  end
+    should "create a new page" do
+      post "/foo.html", :contents => 'fut-fut-fut, freeeeestylo'
+      assert_redirect("/foo.html")
+      follow_redirect!
+      assert last_response.ok?
+      assert last_response.body.include?('freeeeestylo')
+    end
 
-  it "should edit an existent page" do
-    page = page_stub
-    page.save
+    should "edit an existent page" do
+      page = page_stub
+      page.save
 
-    get "#{page.uri}.html.e"
-    assert last_response.ok?
-    assert last_response.body.include?("textarea")
-    assert last_response.body.include?(page.contents)
-  end
+      get "#{page.uri}.html.e"
+      assert last_response.ok?
+      assert last_response.body.include?("textarea")
+      assert last_response.body.include?(page.contents)
+    end
 
-  it "should update an existent page" do
-    page = page_stub
-    page.save
+    should "update an existent page" do
+      page = page_stub
+      page.save
 
-    post "#{page.uri}.html", :contents => "new contents"
-    assert_redirect("#{page.uri}.html")
-    follow_redirect!
-    assert last_response.ok?
-    assert last_response.body.include?("new contents")
-  end
+      post "#{page.uri}.html", :contents => "new contents"
+      assert_redirect("#{page.uri}.html")
+      follow_redirect!
+      assert last_response.ok?
+      assert last_response.body.include?("new contents")
+    end
 
-  it "should provide the edit link" do
-    page = page_stub
-    page.save
+    should "provide the edit-link" do
+      page = page_stub
+      page.save
 
-    get "#{page.uri}.html"
-    assert last_response.ok?
-    assert last_response.body.include?("edit-link")
-    assert last_response.body.include?("#{page.uri}.html.e")
-  end
+      get "#{page.uri}.html"
+      assert last_response.ok?
+      assert last_response.body.include?("edit-link")
+      assert last_response.body.include?("#{page.uri}.html.e")
+    end
 
-  protected
-  def assert_redirect(uri)
-    assert last_response.redirect?
-    assert_equal last_response.headers['Location'], uri
+    protected
+    def assert_redirect(uri)
+      assert last_response.redirect?
+      assert_equal last_response.headers['Location'], uri
+    end
   end
 end
--- test/page_test.rb
+++ test/page_test.rb
@@ -1,99 +1,101 @@
 require 'test_helper'
 
 class PageTest < Test::Unit::TestCase
-  it "should_create_correct_page" do
-    @page = page_stub
-    assert_not_nil @page.contents
-    assert_not_nil @page.title
-    assert_equal 'awesome title', @page.title
-  end
+  describe "Ciridiri::Page" do
+    should "create correct page" do
+      @page = page_stub
+      assert_not_nil @page.contents
+      assert_not_nil @page.title
+      assert_equal 'awesome title', @page.title
+    end
 
-  it "should_save_page" do
-    @page = page_stub
-    assert @page.save
-    assert File.exists?(@page.path)
-    assert File.size(@page.path) > 0
-  end
-
-  it "should_parse_html_and_md_titles" do
-    @page = page_stub
-    @else_one_page = page_stub("index", "<h3 class=\"title\">awesome title</h3>\n hello, everyone!")
-    assert_equal @page.title, "awesome title"
-    assert_equal @page.title, @else_one_page.title
-  end
-
-  it "should_update_page" do
-    @page = page_stub
-    @page.save
-    @page.contents = "#new title\nand new content"
-    assert @page.save
-    assert File.open(@page.path).read.include?('and new content')
-  end
-
-  it "should_find_page_by_uri" do
-    @page = page_stub("hidden/blah")
-    @page.save
-
-    @p = Page.find_by_uri('hidden/blah')
-    assert_not_nil @p
-    assert_not_nil @p.title
-    assert_not_nil @p.contents
-  end
-
-  it "should_return_nil_if_page_is_not_found" do
-    assert_nil Page.find_by_uri('nonexistent-uri')
-  end
-
-  it "should_return_empty_page_if_needed" do
-    assert_not_nil Page.find_by_uri_or_empty('nonexistent-uri')
-  end
-
-  it "should_respect_uri_hierarchy" do
-    @page = page_stub('about/team/boris')
-    @page.save
-
-    target_path = File.expand_path(File.join(Page.content_dir, %w[about team], "boris#{Page::SOURCE_FILE_EXT}"))
-    assert File.exists?(target_path)
-    assert_equal target_path, File.expand_path(@page.path)
-  end
-
-  it "should_create_backups_if_needed" do
-    begin
-      Page.backups = true
+    should "save page" do
       @page = page_stub
       assert @page.save
-      @page.contents = "foo bar"
+      assert File.exists?(@page.path)
+      assert File.size(@page.path) > 0
+    end
+
+    should "parse html and md titles" do
+      @page = page_stub
+      @else_one_page = page_stub("index", "<h3 class=\"title\">awesome title</h3>\n hello, everyone!")
+      assert_equal @page.title, "awesome title"
+      assert_equal @page.title, @else_one_page.title
+    end
+
+    should "update page" do
+      @page = page_stub
+      @page.save
+      @page.contents = "#new title\nand new content"
       assert @page.save
-
-      assert_not_nil @page.revisions
-      assert_equal @page.revisions.length, 1
-    ensure
-      Page.backups = false
+      assert File.open(@page.path).read.include?('and new content')
     end
-  end
 
-  it "should_format_contents" do
-    begin
-      @page = page_stub
+    should "find page by uri" do
+      @page = page_stub("hidden/blah")
       @page.save
-      assert_equal @page.contents, @page.to_html
-      Page.formatter = lambda {|t| "<censored />"}
-      assert_equal @page.to_html, "<censored />"
-    ensure
-      Page.formatter = lambda {|t| t}
+
+      @p = Page.find_by_uri('hidden/blah')
+      assert_not_nil @p
+      assert_not_nil @p.title
+      assert_not_nil @p.contents
     end
-  end
 
-  it "should_cache_page" do
-    begin
-      Page.caching = true
-      @page = page_stub
+    should "return nil if page is not found" do
+      assert_nil Page.find_by_uri('nonexistent-uri')
+    end
+
+    should "return empty page if needed" do
+      assert_not_nil Page.find_by_uri_or_empty('nonexistent-uri')
+    end
+
+    should "respect uri hierarchy" do
+      @page = page_stub('about/team/boris')
       @page.save
-      assert_not_nil @page.to_html
-      assert File.exists?(@page.path + Page::CACHED_FILE_EXT)
-      assert File.size(@page.path + Page::CACHED_FILE_EXT) > 0
-    ensure
-      Page.caching = false
+
+      target_path = File.expand_path(File.join(Page.content_dir, %w[about team], "boris#{Page::SOURCE_FILE_EXT}"))
+      assert File.exists?(target_path)
+      assert_equal target_path, File.expand_path(@page.path)
+    end
+
+    should "create backups if needed" do
+      begin
+        Page.backups = true
+        @page = page_stub
+        assert @page.save
+        @page.contents = "foo bar"
+        assert @page.save
+
+        assert_not_nil @page.revisions
+        assert_equal @page.revisions.length, 1
+      ensure
+        Page.backups = false
+      end
+    end
+
+    should "format contents" do
+      begin
+        @page = page_stub
+        @page.save
+        assert_equal @page.contents, @page.to_html
+        Page.formatter = lambda {|t| "<censored />"}
+        assert_equal @page.to_html, "<censored />"
+      ensure
+        Page.formatter = lambda {|t| t}
+      end
+    end
+
+    should "cache page" do
+      begin
+        Page.caching = true
+        @page = page_stub
+        @page.save
+        assert_not_nil @page.to_html
+        assert File.exists?(@page.path + Page::CACHED_FILE_EXT)
+        assert File.size(@page.path + Page::CACHED_FILE_EXT) > 0
+      ensure
+        Page.caching = false
+      end
     end
   end
 end
